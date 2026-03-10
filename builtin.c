183b3f3c44d7dacb12f557f3a79891e0740f81f2
Carter S. <speers.carter@gmail.com>
2026-03-10 09:38:36 -0400

    feat: added timenow and schedtime builtins

--- builtin.c
+++ builtin.c
@@ -1,19 +1,35 @@
+#define _XOPEN_SOURCE 600
+
 #include "builtin.h"
+#include <time.h>
 #include <stdlib.h>
 
-void (*base_cmnds_ft[BASE_COMMANDS_COUNT])(void*) = { exit_cmnd, echo_cmnd };
-char* base_cmnds_strs[BASE_COMMANDS_COUNT] = { "exit", "echo" };
-
-
+void (*base_cmnds_ft[BASE_COMMANDS_COUNT])(void*) = { exit_cmnd, echo_cmnd, timenow_cmnd, schedtime_cmnd };
+char* base_cmnds_strs[BASE_COMMANDS_COUNT] = { "exit", "echo", "timenow", "schedtime" };
 
 void echo_cmnd(void * args) {
+	char** output = (char**) args;
 
-    char** output = (char**) args;
-
-    printf("%s\n", *output);
+	printf("%s\n", *output);
 }
 
 void exit_cmnd(void * args) {
 
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
+}
+
+void timenow_cmnd(void *args) {
+
+	time_t tm = time(NULL);
+	printf("%s", ctime(&tm));
+}
+
+void schedtime_cmnd(void *args) {
+
+	struct timespec tm;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tm);	
+
+	double secs = (double)tm.tv_sec + (double)tm.tv_nsec / 100'000'000.0;
+
+	printf("-- shell has executed for %f secs on a CPU --\n", secs);
 }
--- builtin.h
+++ builtin.h
@@ -4,15 +4,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BASE_COMMANDS_COUNT (2)
+#define BASE_COMMANDS_COUNT (4)
 
 typedef enum _base_cmnd_t {
     EXIT = 0,
     ECHO = 1,
+		NOW_TM = 2,
+		SCHED_TM = 3,
 } base_cmnd_t;
 
-void echo_cmnd(void * args);
-void exit_cmnd(void * args);
+void echo_cmnd(void *args);
+void exit_cmnd(void *args);
+void timenow_cmnd(void *args);
+void schedtime_cmnd(void *args);
+
 
 extern void (*base_cmnds_ft[BASE_COMMANDS_COUNT])(void*);
 extern char* base_cmnds_strs[BASE_COMMANDS_COUNT];
