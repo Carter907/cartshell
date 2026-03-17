2c076b97aefc85aa2c399e5a9ea707dbfaf94a21
Carter <speers.carter@gmail.com>
2026-03-16 20:52:11 -0400

    chore: clang-format

--- builtin.h
+++ builtin.h
@@ -1,16 +1,16 @@
 #ifndef _BUILTIN_H
 #define _BUILTIN_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BASE_COMMANDS_COUNT (4)
 
 typedef enum _base_cmnd_t {
-	EXIT = 0,
+  EXIT = 0,
   ECHO = 1,
-	NOW_TM = 2,
-	SCHED_TM = 3,
+  NOW_TM = 2,
+  SCHED_TM = 3,
 } base_cmnd_t;
 
 void echo_cmnd(void *args);
@@ -18,9 +18,9 @@ void exit_cmnd(void *args);
 void timenow_cmnd(void *args);
 void schedtime_cmnd(void *args);
 
-typedef void builtin_cmnd(void*);
+typedef void builtin_cmnd(void *);
 
-extern builtin_cmnd* base_cmnds_ft[BASE_COMMANDS_COUNT];
-extern char* base_cmnds_strs[BASE_COMMANDS_COUNT];
+extern builtin_cmnd *base_cmnds_ft[BASE_COMMANDS_COUNT];
+extern char *base_cmnds_strs[BASE_COMMANDS_COUNT];
 
 #endif
--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #define _XOPEN_SOURCE 700
 
 #include <stddef.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "builtin.h"
 
@@ -15,126 +15,123 @@
 
 int read_line(char **ln) {
 
-	size_t ln_sz = 0;
-	size_t buff_cap = LINE_BUFF_SIZE;
+  size_t ln_sz = 0;
+  size_t buff_cap = LINE_BUFF_SIZE;
 
-	*ln = malloc(sizeof(char) * LINE_BUFF_SIZE);
-	
-	int next_char;	
+  *ln = malloc(sizeof(char) * LINE_BUFF_SIZE);
 
-	while ((next_char = getchar()) != EOF) {
-	
-		char ch = (char)next_char;
-		(*ln)[ln_sz++] = ch;
+  int next_char;
 
-		if (ln_sz == buff_cap) {
-			buff_cap *= 2;
-			*ln = realloc(*ln, sizeof(char) * buff_cap);
-		};
+  while ((next_char = getchar()) != EOF) {
 
-		if (next_char == '\n')
-			break;
-	};
-	
-	if (feof(stdin)) {
-		free(*ln);
-		return -1;
-	}
-		
-	
-	(*ln)[ln_sz] = '\0';
+    char ch = (char)next_char;
+    (*ln)[ln_sz++] = ch;
 
-	return 0;
+    if (ln_sz == buff_cap) {
+      buff_cap *= 2;
+      *ln = realloc(*ln, sizeof(char) * buff_cap);
+    };
+
+    if (next_char == '\n')
+      break;
+  };
+
+  if (feof(stdin)) {
+    free(*ln);
+    return -1;
+  }
+
+  (*ln)[ln_sz] = '\0';
+
+  return 0;
 }
-
 
 size_t split_line(char ***split_ln, size_t *split_ln_sz, char *ln) {
 
-  	if (*split_ln == nullptr) {
-		*split_ln = malloc(sizeof(char*) * ARGS_BUFF_SIZE);
-  	}
+  if (*split_ln == nullptr) {
+    *split_ln = malloc(sizeof(char *) * ARGS_BUFF_SIZE);
+  }
 
-	size_t pos = 0;	
-	char* tok = strtok(ln, DELIM_WHITESPACE);
-		
-	do {
-	
-		(*split_ln)[pos] = tok;
-		pos++;
+  size_t pos = 0;
+  char *tok = strtok(ln, DELIM_WHITESPACE);
 
-		if (pos == *split_ln_sz) {
+  do {
 
-			(*split_ln_sz) *= 2;
-			*split_ln = realloc(*split_ln, sizeof(char*) * (*split_ln_sz));			
+    (*split_ln)[pos] = tok;
+    pos++;
 
-			if (*split_ln == nullptr) {
-				perror("realloc()");
-				exit(EXIT_FAILURE);
-			}
-		}
-		
-		tok = strtok(nullptr, DELIM_WHITESPACE);
-		
-	} while (tok != nullptr);
+    if (pos == *split_ln_sz) {
 
-	return pos;
+      (*split_ln_sz) *= 2;
+      *split_ln = realloc(*split_ln, sizeof(char *) * (*split_ln_sz));
+
+      if (*split_ln == nullptr) {
+        perror("realloc()");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    tok = strtok(nullptr, DELIM_WHITESPACE);
+
+  } while (tok != nullptr);
+
+  return pos;
 }
 
 void handle_command(char **split_ln) {
 
-    for (int i = 0; i < BASE_COMMANDS_COUNT; i++) {
-            
-        if (strcmp(split_ln[0], base_cmnds_strs[i]) == 0) {
-            base_cmnds_ft[i]((void*)(split_ln + 1)); 
-        }
+  for (int i = 0; i < BASE_COMMANDS_COUNT; i++) {
+
+    if (strcmp(split_ln[0], base_cmnds_strs[i]) == 0) {
+      base_cmnds_ft[i]((void *)(split_ln + 1));
     }
+  }
 }
 
 int shell_task() {
 
-	char *ln = nullptr;
+  char *ln = nullptr;
 
-	size_t cmnd_cap = ARGS_BUFF_SIZE;
-	[[maybe_unused]]size_t cmnd_size = 0;
+  size_t cmnd_cap = ARGS_BUFF_SIZE;
+  [[maybe_unused]] size_t cmnd_size = 0;
 
-	char **cmnd_split = malloc(sizeof(char*) * cmnd_cap);
+  char **cmnd_split = malloc(sizeof(char *) * cmnd_cap);
 
-	if (cmnd_split == nullptr) {
-		perror("malloc()");
-		exit(EXIT_FAILURE);
-	}
-	
-	while (true) {
+  if (cmnd_split == nullptr) {
+    perror("malloc()");
+    exit(EXIT_FAILURE);
+  }
 
-		printf("> ");
-		int rt = read_line(&ln);
+  while (true) {
 
-		if (rt == -1) {
-			perror("readline()");
-			exit(EXIT_FAILURE);
-		}
+    printf("> ");
+    int rt = read_line(&ln);
 
-		char *input_ln = strdup(ln);
+    if (rt == -1) {
+      perror("readline()");
+      exit(EXIT_FAILURE);
+    }
 
-		cmnd_size = split_line(&cmnd_split, &cmnd_cap, input_ln);
+    char *input_ln = strdup(ln);
 
-    	handle_command(cmnd_split); 
-		
-		free(ln);
-    	ln = nullptr;
-		free(input_ln);
-	    input_ln = nullptr;
-		free(cmnd_split);
-		cmnd_split = nullptr;
-	}
- 
-	return 0;
+    cmnd_size = split_line(&cmnd_split, &cmnd_cap, input_ln);
+
+    handle_command(cmnd_split);
+
+    free(ln);
+    ln = nullptr;
+    free(input_ln);
+    input_ln = nullptr;
+    free(cmnd_split);
+    cmnd_split = nullptr;
+  }
+
+  return 0;
 }
-
 
 int main(int argc, char *argv[]) {
 
-	shell_task();
+  shell_task();
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
